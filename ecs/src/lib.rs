0e380217cd158d0455aebddf9bcdb3f508b086fe
M Farkas-Dyck <strake888@gmail.com>
2018-02-13 16:43:14 -0800

    keep component table in `Components`

--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,52 +1,51 @@
 #![no_std]
 
-#![feature(type_ascription)]
-#![feature(unique)]
-
 extern crate containers;
+extern crate hash_table;
 extern crate loca;
 extern crate ptr as ptr_;
 extern crate siphasher;
+extern crate slot;
 
-use containers::collections::*;
+use containers::collections::RawVec;
 use core::any::TypeId;
-use core::{ptr, slice};
+use core::{mem, ptr, slice};
 use core::ops::*;
+use hash_table::HashTable;
 use loca::*;
 use ptr_::Unique;
 use siphasher::sip;
+use slot::Slot;
 
-type Mask = usize;
+type Mask = u64;
+
+const mask_size: usize = mem::size_of::<Mask>();
+const component_n: usize = mask_size << 3;
 
 pub struct Components<A: Alloc> {
-    alloc: A,
     masks: RawVec<Mask, A>,
-    component_ptrs: HashTable<TypeId, *mut u8, ::sip::SipHasher, A>,
-    droppers: Unique<fn(*mut u8, usize, &mut A)>
+    component_ptrs: HashTable<TypeId, *mut u8, CNArray<usize>, CNArray<Slot<(TypeId, *mut u8)>>,
+                              sip::SipHasher>,
+    droppers: [fn(*mut u8, usize, &mut A); component_n],
 }
 
-impl<A: Alloc + Clone> Components<A> {
+impl<A: Alloc> Components<A> {
     #[inline]
     pub fn with_capacity_in(a: A, cap: usize) -> Option<Self> {
-        Self::with_capacity_(a, cap, Clone::clone)
+        let mut masks = RawVec::with_capacity_in(a, cap)?;
+        for k in 0..cap { unsafe { masks.storage_mut()[k] = 0; } }
+        let cps = HashTable::from_parts(CNArray([0; component_n]),
+                                        unsafe { mem::uninitialized() },
+                                        Default::default());
+        fn no_drop<A>(_: *mut u8, _: usize, _: &mut A) {}
+        Some(Components { masks, component_ptrs: cps, droppers: [no_drop; component_n] })
     }
 }
 
 impl<A: Alloc + Default> Components<A> {
     #[inline]
     pub fn with_capacity(cap: usize) -> Option<Self> {
-        Self::with_capacity_(A::default(), cap, |_| A::default())
-    }
-}
-
-impl<A: Alloc> Components<A> {
-    #[inline(always)]
-    fn with_capacity_<F: Fn(&A) -> A>(mut a: A, cap: usize, f: F) -> Option<Self> {
-        let mut masks = RawVec::with_capacity_in(f(&a), cap)?;
-        for k in 0..cap { unsafe { masks.storage_mut()[k] = 0; } }
-        let cps = HashTable::new_in(f(&a), (0: Mask).trailing_zeros(), Default::default())?;
-        let ds = a.alloc_array(cap).ok()?.0;
-        Some(Components { alloc: a, masks: masks, component_ptrs: cps, droppers: ds })
+        Self::with_capacity_in(A::default(), cap)
     }
 }
 
@@ -54,15 +53,15 @@ impl<A: Alloc> Components<A> {
     /// Register the component type `C`.
     #[inline]
     pub fn reg<C: 'static>(&mut self) -> Result<(), Error> {
-        let alloc = &mut self.alloc;
         let cap = self.masks.capacity();
+        let alloc = unsafe { self.masks.alloc_mut() };
         let ptr: Unique<C> = alloc.alloc_array(cap).map_err(|_| Error(()))?.0;
         match self.component_ptrs.insert_with(TypeId::of::<C>(), |p_opt| match p_opt {
             None => ptr.as_ptr() as _,
             Some(p) => unsafe { let _ = alloc.dealloc_array(ptr, cap); p },
         }).map_err(|(_, ptr)| ptr(None)) {
             Ok((k, _, _)) => unsafe {
-                ptr::write(self.droppers.as_ptr().offset(k as _), drop_components::<C, A>);
+                ptr::write(self.droppers.as_mut_ptr().offset(k as _), drop_components::<C, A>);
                 Ok(())
             },
             Err(ptr) => unsafe {
@@ -112,9 +111,7 @@ pub struct Error(());
 impl<A: Alloc> Drop for Components<A> {
     fn drop(&mut self) {
         for (k, _, &ptr) in self.component_ptrs.iter_with_ix() {
-            if let Some(f) = unsafe { self.droppers.as_ptr().offset(k as _).as_ref() } {
-                f(ptr, self.masks.capacity(), &mut self.alloc);
-            }
+            self.droppers[k](ptr, self.masks.capacity(), unsafe { self.masks.alloc_mut() });
         }
     }
 }
@@ -124,3 +121,39 @@ fn drop_components<C, A: Alloc>(ptr: *mut u8, n: usize, a: &mut A) { unsafe {
     for c in slice::from_raw_parts_mut(ptr, n) { ptr::drop_in_place(c); }
     let _ = a.dealloc_array(Unique::new_unchecked(ptr), n);
 } }
+
+#[derive(Clone, Copy)]
+struct CNArray<A>([A; component_n]);
+
+impl<A> Deref for CNArray<A> {
+    type Target = [A];
+    #[inline]
+    fn deref(&self) -> &[A] { &self.0 }
+}
+
+impl<A> DerefMut for CNArray<A> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut [A] { &mut self.0 }
+}
+
+impl<A> Index<usize> for CNArray<A> {
+    type Output = A;
+    #[inline]
+    fn index(&self, k: usize) -> &A { &self.0[k] }
+}
+
+impl<A> IndexMut<usize> for CNArray<A> {
+    #[inline]
+    fn index_mut(&mut self, k: usize) -> &mut A { &mut self.0[k] }
+}
+
+impl<A> Index<::core::ops::RangeFull> for CNArray<A> {
+    type Output = [A];
+    #[inline]
+    fn index(&self, _: ::core::ops::RangeFull) -> &[A] { &self.0[..] }
+}
+
+impl<A> IndexMut<::core::ops::RangeFull> for CNArray<A> {
+    #[inline]
+    fn index_mut(&mut self, _: ::core::ops::RangeFull) -> &mut [A] { &mut self.0[..] }
+}
